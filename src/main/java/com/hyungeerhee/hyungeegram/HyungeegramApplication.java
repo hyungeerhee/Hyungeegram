31f509e5824a13304d16f4d2a0966e8a633b2f7d
hyungeerhee <hyungeerhee@naver.com>
2024-03-14 23:20:40 +0900

    [이현기] -hyungeegram sns 프로젝트 환경세팅 완료 -git연동 완료

---
+++ src/main/java/com/hyungeerhee/hyungeegram/HyungeegramApplication.java
@@ -0,0 +1,13 @@
+package com.hyungeerhee.hyungeegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyungeegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyungeegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyungeerhee/hyungeegram/HyungeegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyungeerhee.hyungeegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyungeegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
